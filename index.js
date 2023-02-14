6271f849fd070bbd53e4eee3f13bc95bd83143f8
Salmankhansallu <78694695+Salmankhansallu@users.noreply.github.com>
2023-02-14 10:13:43 +0530

    First commit

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const cors=require("cors");
 app.use(cors());
 const port=process.env.PORT || 3000;
 const userdata=require("./userapi.json");
+
 app.get("/",(req,res)=>{
-res.send("hello everyone");
+     //res.write("mmmut user data in the form of api");
+res.send(userdata);
 });
 app.get("/mmmutuser",(req,res)=>{
     res.send(userdata);
